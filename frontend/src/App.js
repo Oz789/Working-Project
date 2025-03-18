7bafe25596d5a6434b4c9b6fe8fd38ebeca55021
davidguzman23 <151577458+davidguzman23@users.noreply.github.com>
2025-03-18 16:44:04 -0500

    backup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import UserNavbar from "./Components/userNavBar";
+import Frames from './pages/Inventory/frames';
+
 
 function App() {
   return (
@@ -8,12 +10,15 @@ function App() {
 
     <Router>
       <UserNavbar />
+      
       <Routes>
         <Route path="/about" element={<h1>About Us Page</h1>} />
         <Route path="/contact" element={<h1>Contact Us Page</h1>} />
         <Route path="/book-appointment" element={<h1>Book Appointment Page</h1>} />
+        <Route path= "/Images"          element={""} />
+        <Route path= "/frames"          element={<Frames />} />
       </Routes>
-        <h1>"Hello World"</h1> 
+        {/* <h1>"Hello World"</h1>  */}
     </Router>
 
 
