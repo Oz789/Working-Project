46a4fe8b275e3e1b03fb54cd3fbd34ea5e6f7f21
matthew-a-reyna <matthewr09@icloud.com>
2025-03-25 17:03:31 -0500

    m

--- frontend/src/Components/NavBar.jsx
+++ frontend/src/Components/NavBar.jsx
@@ -8,7 +8,6 @@ const Navbar = () => {
   
   const [productsDropdown, setProductsDropdown] = useState(false);
 
-  return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar sx={{ backgroundColor: "#fffbed", height: "65px", boxShadow: "brown", borderBottom: "2px solid #8B5A2B" }} position="static">
         <Toolbar sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
